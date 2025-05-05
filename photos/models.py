98304ba0558e4ac61a10fe8e4769ce56a0381972
Dima <aldmikon@gmail.com>
2025-05-05 08:49:17 +0300

    Creating 'photos' model, applying migrations

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    image_random_num = models.IntegerField(default=None)
+
+    def __str__(self):
+        return f"Image's random number{self.image_random_num}"
+
+
+
