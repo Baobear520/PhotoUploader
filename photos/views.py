5b04cd6bedd65893028a9c1cc9810b1333a554c7
Dima <aldmikon@gmail.com>
2025-05-05 08:58:17 +0300

    Adding admin site

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.contrib import admin
+from django.urls import path
 
-# Create your views here.
+urlpatterns = [
+    path("admin/", admin.site.urls),
+]
