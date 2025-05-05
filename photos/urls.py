06c185c357a1abb90e301cc14045dc563e0b011a
Dima <aldmikon@gmail.com>
2025-05-05 13:42:00 +0300

    Altering the url for upload_photo view

--- photos/urls.py
+++ photos/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from photos import views
 
 urlpatterns = [
-    path('', views.upload_photo, name='upload_photo'),
+    path('photo', views.upload_photo, name='upload_photo'),
+
 ]
