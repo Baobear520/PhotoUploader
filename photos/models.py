9a037bd87acb05f4a8ca11f3dd8fe5a64f8df762
Dima <aldmikon@gmail.com>
2025-05-07 22:43:07 +0300

    Changing model field file -> file_name

--- photos/models.py
+++ photos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class ImageRecord(models.Model):
-    file_name = models.ImageField()
+    file_name = models.TextField()
     image_random_num = models.IntegerField(default=None)
 
     def __str__(self):
