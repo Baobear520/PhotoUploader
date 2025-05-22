255e575a00c243b58c4418847cf3d4d6e781f6d4
Dima <aldmikon@gmail.com>
2025-05-22 14:23:51 +0300

    Implementing send_alert group task

--- photos/tasks.py
+++ photos/tasks.py
@@ -1,15 +1,15 @@
 import os
+from importlib import import_module
 
+from django.conf import settings
 from django.db import OperationalError
 from django.forms.models import model_to_dict
-from celery import shared_task
+from celery import shared_task, group
 from urllib3.exceptions import NameResolutionError
 
-from .handlers import image_handler
+from .handlers import image_handler, get_handler
 from .models import ImageRecord
 import logging
-
-from .notifications import send_alert, telegram_sender
 
 
 logger = logging.getLogger(__name__)
@@ -19,13 +19,18 @@ def image_task(self, file_name: str) -> dict | None:
     try:
         num, execution_time = image_handler(file_name)
         record = ImageRecord.objects.create(file_name=file_name, image_random_num=num)
-        logger.info(f"Record to save: {record}")
         data = model_to_dict(record)
         data["execution_time"] = execution_time
-        msg = f"Task completed: {data}"
+
+        msg = f"Task {self.request.id}: {data}"
         logger.info(msg, extra=data)
 
-        send_alert(telegram_sender, message=msg)
+        # Send notifications to all backends
+        alert_group = group(
+            send_alert_task.s(backend, msg) for backend in settings.NOTIFICATION_BACKENDS
+        )
+        alert_group.delay()  # Запускаем все таски параллельно
+
         return data
 
     except OperationalError as exc:
@@ -36,6 +41,19 @@ def image_task(self, file_name: str) -> dict | None:
     except Exception as e:
         logger.error(f"{type(e).__name__}: {e}")
 
+
+@shared_task
+def send_alert_task(backend: str, message: str):
+    """Универсальная таска для отправки алертов."""
+    try:
+        handler = get_handler(backend)
+        success = handler.send(message)
+
+        if not success:
+            raise Exception(f"{backend} alert failed")
+    except Exception as e:
+        logger.error(f"Alert error: {e}")
+        raise
 
 
 
